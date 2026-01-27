890880ae05cf0f7c7ab144287e57b4f818ed513b
ArunVKammath <arunvenugopalkammath97@gmail.com>
2026-01-28 02:23:16 +0530

    created basic functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 
 import './App.css'
 import Tile from './components/Tile.jsx'
-
+import { useSymbol } from './hooks/Symbol.jsx'
 function App() {
+  const { symbol, setSymbol } = useSymbol(); 
 
   return(
     <div className='layout'>
-      {[...Array(9)].map((_, i) => (
-        <Tile key={i} />
-      ))}
+      <h1>Tic Tac Toe</h1>
+      <p>Next Player: {symbol}</p>
+      <div className='board'>   
+        {[...Array(9)].map((_, i) => (
+          <Tile key={i} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,14 +1,24 @@
-import { useSymbol } from '../hooks/Symbol.jsx';    
+  
 import '../style/Tile.css';
+import { useRef } from 'react';
+import { useSymbol } from '../hooks/Symbol.jsx';
 
 function Tile(){
-    const {symbol, setSymbol} = useSymbol();
-
+    const { symbol, setSymbol } = useSymbol();
+    const symbolRef = useRef(null);
+    const clickedRef = useRef(false);
+    const handleClick = () => {
+        if(!clickedRef.current){
+            symbolRef.current = symbol;
+            clickedRef.current = true;
+            setSymbol(symbol === 'X' ? 'O' : 'X');
+        }
+    };
     return(
         <div className="tile">
-            <p>{symbol}</p>
+            <button ref={symbolRef} onClick={handleClick}>{clickedRef.current ? symbolRef.current : ' '}</button>
         </div>
-    )
+    );
 }
 
-export default Tile
+export default Tile;
