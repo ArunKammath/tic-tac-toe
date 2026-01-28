3744c650debd1099c9e1237c828efba101026205
ArunVKammath <arunvenugopalkammath97@gmail.com>
2026-01-28 13:04:21 +0530

    IMproved the css of the game

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,12 +1,16 @@
   
 import '../style/Tile.css';
 
-function Tile({ board, rowIndex, cellIndex, onClick }){
-    return(
-        <div className='tile'>
-            <button className='tile-button' onClick={onClick}>{board[rowIndex][cellIndex]}</button>
-        </div>
-    );
+function Tile({ board, rowIndex, cellIndex, onClick }) {
+  const value = board[rowIndex][cellIndex];
+  const markClass = value === 'X' ? 'tile-x' : value === 'O' ? 'tile-o' : '';
+  return (
+    <div className="tile">
+      <button className={`tile-button ${markClass}`} onClick={onClick}>
+        {value || '\u00A0'}
+      </button>
+    </div>
+  );
 }
 
 export default Tile;
